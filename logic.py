f86385504d28859fdc4c3c338359db9b2302cca2
Vasily Levit <vasily.levit@gmail.com>
2022-09-28 22:55:38 +0800

    make arhitectura and first logic

---
+++ logic.py
@@ -0,0 +1,8 @@
+import ui
+import book_save as bs
+
+def button_click():
+    if ui.get_comand() == '1':
+        bs.get_data(ui.get_data())
+
+
---
+++ ui.py
@@ -0,0 +1,6 @@
+def get_comand():
+    print('1 - добавление записи в книгу')
+    return input('Введите номер операции: ')
+
+def get_data():
+    return input('Введите фамилию: ')
