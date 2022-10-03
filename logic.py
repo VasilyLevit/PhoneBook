0e8c32ce85e1c4c66bc2e99c508c23fb7902a295
Vasily Levit <vasily.levit@gmail.com>
2022-10-03 14:22:25 +0800

    modified loop program with exit

--- logic.py
+++ logic.py
@@ -3,8 +3,14 @@ import book_save as bs
 import book_find as bf
 
 def button_click():
-    operation = ui.get_comand()
-    if operation == '1':
-        bs.log_data(ui.get_data())
-    elif operation == '2':
-        ui.print_data(bf.find_data(ui.get_find_string()))
+    operation = 0
+    while operation != '4':
+        operation = ui.get_comand()
+        if operation == '1':
+            bs.log_data(ui.get_data())
+            operation = ui.get_comand()
+        elif operation == '2':
+            ui.print_data(bf.find_data(ui.get_find_string()))
+            operation = ui.get_comand()
+        else:
+            operation = '4'
