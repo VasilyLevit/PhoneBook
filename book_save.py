c19a3f67c54b1cd44e99d87c2a80d121a032186e
Vasily Levit <vasily.levit@gmail.com>
2022-10-03 16:14:44 +0800

    modified ligth

--- book_save.py
+++ book_save.py
@@ -1,6 +1,6 @@
 import csv
 
 def log_data(entry):
-    with open('book.csv', 'a') as file:
+    with open('book.csv', 'a', newline='') as file:
         data = csv.writer(file, delimiter = ',')
         data.writerow(entry)
